cc958a56141c20feac59e0b51c7923f7b9f6ebf4
aetherspritee <schautendustin@gmail.com>
2024-04-22 11:38:30 +0200

    265 monomers

--- virga/test_particle.py
+++ virga/test_particle.py
@@ -3,5 +3,5 @@ from particle_generator import ParticleGenerator
 from pathlib import Path
 
 p = ParticleGenerator()
-res = p.aggregate_generator(radius=1.4,refrind=1.0+0.0j, df=1.7, N=4096, directory=Path("/home/dsc/master/virga"))
+res = p.aggregate_generator(radius=1.4,refrind=1.0+0.0j, df=1.7, N=256, directory=Path("/home/dsc/master/virga"))
 print(res)
